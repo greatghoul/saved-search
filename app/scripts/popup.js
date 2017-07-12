719adf24ea88885afc72e1e64afa4e7a8eaad5aa
greatghoul <greatghoul@gmail.com>
2017-07-13 02:54:02 +0800

    Fetch current search

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -1,6 +1,29 @@
 import angular from 'angular';
 
-var app = angular.module('app', []);
+const URL_PAT = /https?:\/\/www\.google\..*\/search\?.*/;
+
+const app = angular.module('app', []);
 app.controller('PopupCtrl', $scope => {
-  $scope.foo = 'bar';
+  $scope.search = null;
+
+  $scope.init = function() {
+    $scope.activeTab().then(tab => {
+      $scope.$apply(() => $scope.search = { url: tab.url, name: $scope.keywords(tab.url) });
+    });
+  };
+
+  $scope.keywords = function(url) {
+    const searchParams = new URLSearchParams(url.split('?').pop());
+    return searchParams.get('q');
+  }
+
+  $scope.activeTab = function() {
+    return new Promise((resolve, reject) => {
+      chrome.tabs.query({ active: true }, tabs => {
+        if (tabs && tabs.length > 0 && URL_PAT.test(tabs[0].url)) {
+          resolve(tabs[0]);
+        }
+      });
+    });
+  };
 });
