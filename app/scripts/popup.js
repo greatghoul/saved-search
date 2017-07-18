e5b6209d9e5425f94ba52a1fbe83bc7ffac7ecdd
greatghoul <greatghoul@gmail.com>
2017-07-19 00:21:14 +0800

    Improve list group styles

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -1,20 +1,45 @@
 import angular from 'angular';
+import _ from 'lodash';
 
 const URL_PAT = /https?:\/\/www\.google\..*\/search\?.*/;
 
 const app = angular.module('app', []);
-app.controller('PopupCtrl', $scope => {
+app.controller('PopupCtrl', function($scope) {
+  $scope.name = null;
   $scope.search = null;
-  $scope.showNew = false;
+  $scope.persisted = false;
+  $scope.showSaveModal = false;
+  $scope.showNameDropdown = false;
+  $scope.searches = [];
 
   $scope.init = function() {
+    $scope.loadSavedSearches();
+
     $scope.activeTab().then(tab => {
       $scope.$apply(() => {
-        $scope.search = { url: tab.url, name: $scope.keywords(tab.url) };
-        $scope.showNew = true;
+        const keywords = $scope.keywords(tab.url);
+        const search = { url: tab.url, name: keywords, keywords: keywords };
+        $scope.openSaveModal(search, false);
       });
     });
-  };
+  }
+
+  $scope.toggleNameDropdown = function(show) {
+    $scope.showNameDropdown = show;
+  }
+
+  $scope.selectSavedSearch = function(name) {
+    $scope.search.name = name;
+    $scope.toggleNameDropdown(false);
+  }
+
+  $scope.loadSavedSearches = function() {
+    for (const name in localStorage) {
+      if (/^search_.*$/.test(name)) {
+        $scope.searches.push(JSON.parse(localStorage[name]));
+      }
+    }
+  }
 
   $scope.keywords = function(url) {
     const searchParams = new URLSearchParams(url.split('?').pop());
@@ -31,11 +56,32 @@ app.controller('PopupCtrl', $scope => {
     });
   }
 
-  $scope.cancelNewSearch = function() {
-    $scope.showNew = false;
+  $scope.openSaveModal = function(search, persisted) {
+    $scope.persisted = persisted;
+    $scope.name = search.name;
+    $scope.search = search;
+    $scope.showSaveModal = true;
   }
 
-  $scope.saveNewSearch = function() {
-    $scope.showNew = false;
+  $scope.closeSaveModal = function() {
+    $scope.showSaveModal = false;
+  }
+
+  $scope.saveSearch = function() {
+    if ($scope.persisted) {
+      delete localStorage[`search_${$scope.name}`];
+      const index = _.findIndex($scope.searches, { name: $scope.name });
+      $scope.searches.splice(index, 1);
+    }
+
+    localStorage[`search_${$scope.search.name}`] = JSON.stringify($scope.search);
+    const search = _.find($scope.searches, { name: $scope.search.name });
+    if (search) {
+      _.assign(search, $scope.search);
+    } else {
+      $scope.searches.push($scope.search);
+    }
+
+    $scope.closeSaveModal();
   }
 });
