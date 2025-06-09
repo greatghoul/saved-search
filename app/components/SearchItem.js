d4112cc2af73544ac3c0cc84e5ca33dda4799bfb
greatghoul <greatghoul@gmail.com>
2025-06-09 21:13:26 +0800

    Add search item open link and enhance keyword display in SearchItem component

--- app/components/SearchItem.js
+++ app/components/SearchItem.js
@@ -6,7 +6,7 @@ function SearchItem({ search, onEdit }) {
     <li class="list-group-item d-flex flex-column align-items-start search-item">
       <div class="d-flex w-100 justify-content-between align-items-center">
         <span class="text-secondary-emphasis text-truncate" title=${search.name}>${search.name}</span>
-        <a class="text-success" href="#" onClick=${onEdit}>
+        <a class="text-success" href=${search.url} target="_blank" rel="noopener noreferrer" title=${i18n('searchItemOpen')}>
           <i class="bi bi-box-arrow-up-right"></i>
         </a>
       </div>
